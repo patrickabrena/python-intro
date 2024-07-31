cdd92490d7e294f2b0b8014e64dc4d8540850b40
Patrick Abrena <patrickabrena@hotmail.com>
2024-07-30 19:30:26 -0500

    add python_syntax.py to demonstrate indentation

---
+++ python_syntax.py
@@ -0,0 +1,17 @@
+#Learning how white space affects python
+x = 1 + 2
+print(x)
+
+
+#use backslash If I'm going to add an operator and operand on a different line
+y = 1 +      2 \
++ 3
+
+print(y)
+# when running the program it will produce 6 for print y
+
+#Indentation example
+name = "john"
+if name == "john":
+    print(name)
+# to make this print statement work, I need to have an indentation with 
