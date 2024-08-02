42a064ea398f6923616e42161f01091d33b53b90
Patrick Abrena <patrickabrena@hotmail.com>
2024-08-02 11:27:25 -0500

    fininsh type_casting.py explanation

---
+++ type_casting.py
@@ -0,0 +1,12 @@
+#TYPE CASTING NOTES
+
+#typecasting is the process of converting one data type to antoher. 
+#python has two types of conversions; implicit and explicit
+
+#implict data type is performed automatically by python's complier.
+#example of this is converting an into to float if it picks up the inserted value with a decimal
+
+#explicit data type conversion uses provided python functions.
+# str()
+# int()
+# float()
